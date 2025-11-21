276a9cf2a3d812a0c3607202024698c0d94bc474
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 15:48:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
+import Products from './components/Products'
+import WhyChoose from './components/WhyChoose'
+import Industries from './components/Industries'
+import Process from './components/Process'
 import Footer from './components/Footer'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      <Header />
       <Hero />
       <About />
+      <Products />
+      <WhyChoose />
+      <Industries />
+      <Process />
       <Footer />
     </div>
   )
