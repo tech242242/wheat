8ac1ffb0f13f98419ab9138ba20a177a8615999e
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 16:41:56 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import LoadingAnimation from './components/LoadingAnimation'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
+import StatsCounter from './components/StatsCounter'
 import Products from './components/Products'
 import WhyChoose from './components/WhyChoose'
 import Industries from './components/Industries'
 import Process from './components/Process'
+import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css'
@@ -13,13 +16,16 @@ import './App.css'
 function App() {
   return (
     <div className="app">
+      <LoadingAnimation />
       <Header />
       <Hero />
       <About />
+      <StatsCounter />
       <Products />
       <WhyChoose />
       <Industries />
       <Process />
+      <Testimonials />
       <Contact />
       <Footer />
     </div>
